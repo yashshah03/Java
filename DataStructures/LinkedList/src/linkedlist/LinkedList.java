d4664e6dfd79dba554b176fc8604ce359c71d184
Yash Shah <yashshah2692@hotmail.com>
2018-12-27 23:12:41 -0500

    Adds add/remove methods and tests

--- DataStructures/LinkedList/src/linkedlist/LinkedList.java
+++ DataStructures/LinkedList/src/linkedlist/LinkedList.java
@@ -11,14 +11,13 @@ public class LinkedList<T>
     private LinkedListNode<T> current = null;
     private int size = 0;
     /**
-    * Pushes new element to the list. 
+    * Adds new element to the end of the list. 
     *
     * @param  value Element to be added
-    * @see         LinkedListNode
+    * @see          LinkedListNode
     */ 
-    public void push(T value)
+    public void add(T value)
     {
-        // If HEAD is populated
         if(head!=null)
         {
             LinkedListNode<T> cNode = new LinkedListNode<>(value);
@@ -33,7 +32,113 @@ public class LinkedList<T>
         }
         size++;
     }
-    
+    /**
+    * Adds new element to the list at given index, except at the end of list.
+    * To push element at the end of list use regular Add method without index.
+    * <p>
+    * The list must be initialized and have at least one element.
+    *
+    * @param  value Element to be added
+    * @param  index index for the new element
+    * @see          LinkedListNode
+    * @see          LinkedList.add()
+    */ 
+    public void add(T value, int index)
+    {
+        if(index == size)
+        {
+            System.err.println("[ERROR] Cannot enter element at the end"
+                    + ", To add at the end, use push without index");
+            return;
+        }
+        if(index > size)
+        {
+            System.err.println("[ERROR] Cannot enter element at given index"
+                    + ", index must be smaller than list size");
+            return;
+        }
+        
+        LinkedListNode<T> nNode = new LinkedListNode<>(value);
+        LinkedListNode<T> cNode = head;
+        LinkedListNode<T> pNode = cNode;
+        int i = 0;
+        while(i++ < index)
+        {
+            pNode = cNode;
+            cNode = cNode.getNext();
+        }
+        nNode.setNext(cNode);
+        if(index > 0) pNode.setNext(nNode);
+        if(index == 0) head = nNode;
+        size++;
+    }
+    /**
+    * Removes the head element out of the list. 
+    *
+    * @return  Removed linkedListNode
+    * @see     LinkedListNode
+    */ 
+    public LinkedListNode<T> remove()
+    {
+        if(head!=null)
+        {
+            LinkedListNode<T> rNode = head;
+            head = head.getNext();
+            size--;
+            return rNode;
+        }
+        else
+        {
+            System.err.println("[ERROR] Cannot remove, list empty");
+            return null;
+        }
+    }
+    /**
+    * Removes element with given value out of the list.
+    * <p>
+    * If multiple elements exists with given value, method removes first 
+    * element found with the value
+    *
+    * @param  value Element to be removed
+    * @return  Removed linkedListNode
+    * @see     LinkedListNode
+    */ 
+    public LinkedListNode<T> remove(T value)
+    {
+        if(head!=null)
+        {
+            LinkedListNode<T> cNode = head;
+            LinkedListNode<T> pNode = cNode;
+            while(cNode != null && cNode.getValue() != value)
+            {
+                pNode = cNode;
+                cNode = cNode.getNext();
+            }
+            if(cNode == null)
+            {
+                System.err.println("[ERROR] Cannot remove, No element found");
+                return null;
+            }
+            if(cNode == head)
+                head = head.getNext();
+            else
+                pNode.setNext(cNode.getNext());            
+            
+            size--;
+            return cNode;
+        }
+        else
+        {
+            System.err.println("[ERROR] Cannot remove, list empty");
+            return null;
+        }
+    }
+    /**
+    * Get the list element in form of a string
+    *
+    * @return  list elements represented as string
+    * @see     LinkedList
+    */
     public String getListValue()
     {
         String retValue = "";
--- DataStructures/LinkedList/test/linkedlist/LinkedListTest.java
+++ DataStructures/LinkedList/test/linkedlist/LinkedListTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package linkedlist;
 
 import static org.junit.Assert.assertEquals;
@@ -10,20 +5,67 @@ import org.junit.Test;
 
 /**
  *
- * @author yashs
+ * @author Yash Shah
  */
 public class LinkedListTest 
 {
     @Test
-    public void testPush() 
+    public void testAdd() 
     {
-        System.out.println("push");
+        System.out.println("Add");
         LinkedList<String> list = new LinkedList<>();        
-        list.push("A");
-        list.push("B");
-        list.push("C");
-        list.push("D");
-        list.push("A");  
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("D");
+        list.add("A");  
         assertEquals("ABCDA", list.getListValue());
+    }
+    @Test
+    public void testAddAtIndex() 
+    {
+        System.out.println("Add at index");
+        LinkedList<String> list = new LinkedList<>();        
+        list.add("A");
+        list.add("B");
+        list.add("C", 0);
+        list.add("D", 1);
+        list.add("A");  
+        assertEquals("CDABA", list.getListValue());
+    }
+    @Test
+    public void testRemove() 
+    {
+        System.out.println("Remove");
+        LinkedList<String> list = new LinkedList<>();        
+        list.add("A");
+        list.add("B");
+        list.add("A");
+        list.remove();
+        list.remove();
+        list.remove(); 
+        list.remove(); // List empty  
+        assertEquals("", list.getListValue());
+    }
+    @Test
+    public void testRemoveValue() 
+    {
+        System.out.println("Remove value");
+        LinkedList<String> list = new LinkedList<>();        
+        list.add("A");
+        list.add("B");
+        list.add("C", 0);
+        list.add("D", 1);
+        list.add("A");
+        list.add("B");
+        list.remove("A");
+        list.remove("C");
+        list.remove("B");
+        list.remove("D");
+        list.remove("A"); 
+        list.remove("F"); // Not in list
+        list.remove("B");
+        list.remove("B"); // List empty  
+        assertEquals("", list.getListValue());
     }
 }
