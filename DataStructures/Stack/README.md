6cee19ea06c04f138f8e3fce435117c83965a375
Yash Shah <yashshah2692@hotmail.com>
2018-12-29 17:02:41 -0500

    Creates initial stack program

---
+++ DataStructures/Stack/README.md
@@ -0,0 +1,20 @@
+## This project contains sample implementation of a stack in java
+   **The project is developed using Apache Netbeans IDE 10.0, Java 8, Windows 10, Junit4.12**
+
+**_This project uses my implementation of a [LinkedList](https://github.com/yashshah03/Java/tree/LinkedList/DataStructures/LinkedList) as the base DataStructure._**
+
+## Features:
+   - [X] Generic support (Can be used with any data types).
+   - [ ] Thread safe
+
+## Methods:
+   - push(value)
+     * Adds given value at the top of the stack.
+   - pop()
+     * Removes the value at the top.
+   - peek()
+     * Retrieve but not remove the value at top.
+   - size()
+     * Get the size of the stack.
+   - value()
+     * Get the values of all nodes in the stack represented as a string.
---
+++ DataStructures/Stack/src/stack/Stack.java
@@ -0,0 +1,62 @@
+package stack;
+
+import linkedlist.LinkedList;
+/**
+ *
+ * @author Yash Shah
+ * @param <T>
+ */
+public class Stack<T>
+{
+    private LinkedList<T> list;
+    public Stack() 
+    {
+        this.list = new LinkedList<>();
+    }
+    
+    /**
+     * Insert element in stack at the top position
+     * 
+     * @param value Element to be inserted
+     */
+    public void push(T value)
+    {
+        list.add(value);
+    }
+    /**
+     * Remove top element from stack
+     * 
+     * @return Element removed
+     */
+    public T pop()
+    {
+        return list.remove();
+    }
+    /**
+     * Retrieve but don't remove the first element from queue. 
+     * 
+     * @return The first element in queue, null if queue empty
+     */
+    public T peek()
+    {
+        return list.getCurrent();
+    }
+    /**
+     * Retrieve queue size. 
+     * 
+     * @return The size of the queue.
+     */
+    public int size()
+    {
+        return list.getListSize();
+    }
+    /**
+     * Get the contents of queue. 
+     * 
+     * @return Contents of queue in string format.
+     */
+    public String value()
+    {
+        return list.getListValue();
+    }
+}
